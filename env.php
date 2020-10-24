b68914fc62a65683b5be2f46c868a6ce65339788
root <root@ip-172-31-17-86.us-west-2.compute.internal>
2020-10-23 22:05:08 -0600

    update env file

--- env.php
+++ env.php
@@ -1,9 +1,7 @@
 <?php
 
-$apikey = "16398-REDACTED-87affa6d53a";
-$apidomain = "demo.sparkpostelite.com";
+$apikey = "d7d098b5d629be88055e5afb302d214bd0319c41";
+$apidomain = "api.sparkpost.com";
 $contactlink = "./contact.php";
 $apptitle = "Message Assembler for SparkPost";
-
-//https://api.sparkpost.com/api/v1
 
